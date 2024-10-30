8f2b31c06c460472ae63d5da8309a777b92658c2
xiangdi18 <57522050+xiangdi18@users.noreply.github.com>
2024-10-30 16:12:40 +0800

    Update method.py

--- pages/method.py
+++ pages/method.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+# Add custom HTML and CSS to style the caption 
+st.markdown( """ <style> .caption { font-size: 20px; font-family: Arial, sans-serif; color: blue; } </style> """, unsafe_allow_html=True) 
+
+
+
+
+st.title("Methodology (Flow Chart)")
+st.image("method.jpg")
+st.markdown('<div class="caption">Signature verfication flow</div>', unsafe_allow_html=True)
+
+
 
